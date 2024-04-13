1461ac64c3f63ad615a39c68e7384c244bf5d149
RonaldoAraujo2023 <143138115+RonaldoAraujo2023@users.noreply.github.com>
2024-04-13 11:34:10 -0300

    continuando as variaves

---
+++ tipos-variaveis/src/TiposVariaves.java
@@ -0,0 +1,22 @@
+public class TiposVariaves {
+    public static void main(String[] args) throws Exception {
+       // System.out.println("Hello, World!");//
+         double salarioMinimo =2500;
+
+         short numeroCurto =1;
+         int numeroNormal = numeroCurto;
+         short numeroCurto2 =  (short) numeroNormal;
+
+         int numero = 5;
+         numero = 10;
+
+         system.out.print(numero);
+
+         final double VALOR DE PI = 3.14;
+
+         
+
+
+             
+    }
+}
